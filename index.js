b71c4eedeb18bd6c103101560e006ea4570bc052
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-11-07 12:53:04 -0800

    Copied frm older version. Added test coverage for untests code + some refactoring.

---
+++ index.js
@@ -0,0 +1,177 @@
+module.exports.title = "Change of Base Free Response";
+
+module.exports.defaultConversions = [ 
+    { radix:{ from: 10, to: 2 }, range:{ min: 0, max: 255} },
+    { radix:{ from: 2, to: 10 }, range:{ min: 0, max: 255} },
+    { radix:{ from: 2, to: 8 }, range:{ min: 0, max: 511 } },
+    { radix:{ from: 8, to: 2 }, range:{ min: 0, max: 63 } },
+    { radix:{ from: 2, to: 16 }, range:{ min: 0, max: 65535} },
+    { radix:{ from: 16, to: 2 }, range:{ min: 0, max: 65535} } 
+];
+
+module.exports.radixDescription = function (radix, useBase) {
+    if (useBase)
+        return "base " + radix;
+    switch(radix) {
+        case 8: return "octal";
+        case 16: return "hexadecimal";
+        case 2: return "binary";
+        case 10: return "decimal";
+        default: return "base " + radix;
+    }
+}
+
+
+module.exports.getConversion = function(randomStream, params, defaultValue) {
+    var conversions = defaultValue;
+    if (params && params.conversions && params.conversions.length > 0)
+    	conversions = params.conversions;
+    return randomStream.pick(conversions);
+}
+
+module.exports.formatFrom = function(from, fromRad, toRad) {
+    if (fromRad != 2 || (toRad != 8 && toRad != 16)) 
+    	return from;
+    var groupSize = (toRad == 8) ? 3 : 4;
+    return formatBinary(from, groupSize);
+}
+
+module.exports.formatAnswer = function(answer, fromRad, toRad) {
+    if (toRad != 2 || (fromRad != 8 && fromRad != 16))
+    	return answer;
+    var groupSize = (fromRad == 8) ? 3 : 4;
+    return formatBinary(answer, groupSize);
+}
+
+function nZeros(n) {
+    return Array(n+1).join("0");
+}
+
+function formatBinary(str, groupSize) {
+    if (str.length % groupSize != 0)
+        str = nZeros(groupSize - str.length % groupSize) + str;
+    return str.match(new RegExp('.{1,' + groupSize + '}', 'g')).join(" ");
+}
+
+
+
+module.exports.generateQuestionText = function (randomStream, from, fromRad, toRad, spaceBinary) {
+    var fromDesc = module.exports.radixDescription(fromRad, randomStream.nextIntRange(2));
+    var toDesc = module.exports.radixDescription(toRad, randomStream.nextIntRange(2));
+    if (spaceBinary)
+    	from = module.exports.formatFrom(from, fromRad, toRad);
+    return "Convert " + from + " from " + fromDesc + " to " + toDesc + ".";
+}
+
+module.exports.getSpaceBinary = function(params) {
+	return (params && 'spaceBinary' in params) ? params.spaceBinary : true;
+}
+
+module.exports.generate = function(randomStream, params) {
+	var conversion = module.exports.getConversion(randomStream, params, module.exports.defaultConversions);
+	var spaceBinary = module.exports.getSpaceBinary(params);
+    var numToConvert = randomStream.randIntBetweenInclusive(conversion.range.min, conversion.range.max);
+    var fromRad = conversion.radix.from;
+    var toRad = conversion.radix.to;      
+    var from = numToConvert.toString(fromRad);
+
+
+	this.answer = numToConvert.toString(toRad);
+    if (spaceBinary)
+    	this.answer = module.exports.formatAnswer(this.answer, fromRad, toRad);
+    
+	this.question = module.exports.generateQuestionText(randomStream, from, fromRad, toRad, spaceBinary);
+	this.format = 'free-response';
+	
+};
+
+	
+
+module.exports.validateParameters = function(params) {var errors = [];
+    if (params === undefined) return [];
+    if (typeof params !== 'object') return [{ type: 'ExpectedObjectError', path: []}];
+
+    // spaceBinary validation
+    if ('spaceBinary' in params) {
+        if (params.spaceBinary !== true && params.spaceBinary !== false)
+            errors.unshift({ type: 'ExpectedBooleanError', path: ['spaceBinary']});
+    }
+    
+    // conversions validation
+    if ('conversions' in params) {
+        if (!Array.isArray(params.conversions)) {
+            errors.unshift({ type: 'ExpectedArrayError', path: ['conversions']});
+        } else {
+            for (var i = 0; params.conversions.length > i; i++) {
+                var conversion = params.conversions[i];
+
+                if (!('radix' in conversion)) {
+                    errors.unshift({ type: 'RequiredError', path: ['conversions', i, 'radix']});
+                } else {
+                    // must be [2-10] | [16]
+                    if (!('from' in conversion.radix)) {
+                        errors.unshift({ type: 'RequiredError', path: ['conversions', i, 'radix', 'from']});
+                    } else {
+                        if (!(Number.isSafeInteger(conversion.radix.from))) 
+                            errors.unshift({ type: 'ExpectedIntegerError', path: ['conversions', i, 'radix', 'from']});
+                        if (conversion.radix.from < 2) 
+                            errors.unshift({ type: 'MinimumValueError', path: ['conversions', i, 'radix', 'from']});
+                        if (conversion.radix.from > 36)
+                            errors.unshift({ type: 'MaximumValueError', path: ['conversions', i, 'radix', 'from']});
+                    }
+
+                    if (!('to' in conversion.radix)) {
+                        errors.unshift({ type: 'RequiredError', path: ['conversions', i, 'radix', 'to']});
+                    } else {
+                        if (!(Number.isSafeInteger(conversion.radix.to))) 
+                            errors.unshift({ type: 'ExpectedIntegerError', path: ['conversions', i, 'radix', 'to']});
+                        if (conversion.radix.to < 2) 
+                            errors.unshift({ type: 'MinimumValueError', path: ['conversions', i, 'radix', 'to']});
+                        if (conversion.radix.to > 36)
+                            errors.unshift({ type: 'MaximumValueError', path: ['conversions', i, 'radix', 'to']});
+                    }
+
+                    if (('from' in conversion.radix) && ('to' in conversion.radix)) {
+                        if (conversion.radix.from === conversion.radix.to) {
+                            errors.unshift({ type: 'ToFromEqualError', path: ['conversions', i, 'radix']});
+                        }
+                    }
+                }
+
+                if (!('range' in conversion)) {
+                    errors.unshift({ type: 'RequiredError', path: ['conversions', i, 'range']});
+                } else {
+                    if (!('min' in conversion.range)) {
+                        errors.unshift({ type: 'RequiredError', path: ['conversions', i, 'range', 'min']});
+                    } else {
+                        if (!(Number.isSafeInteger(conversion.range.min))) 
+                            errors.unshift({ type: 'ExpectedIntegerError', path: ['conversions', i, 'range', 'min']});
+                        if (conversion.range.min < 0) 
+                            errors.unshift({ type: 'MinimumValueError', path: ['conversions', i, 'range', 'min']});
+                    }
+
+                    if (!('max' in conversion.range)) {
+                        errors.unshift({ type: 'RequiredError', path: ['conversions', i, 'range', 'max']});
+                    } else {
+                        if (!(Number.isSafeInteger(conversion.range.max))) 
+                            errors.unshift({ type: 'ExpectedIntegerError', path: ['conversions', i, 'range', 'max']});
+                        if (conversion.range.max < 0) 
+                            errors.unshift({ type: 'MinimumValueError', path: ['conversions', i, 'range', 'max']});
+                    }
+
+                    if (('max' in conversion.range) && ('min' in conversion.range)) {
+                        if (conversion.range.min > conversion.range.max) {
+                            errors.unshift({ type: 'InvalidIntervalError', path: ['conversions', i, 'range']});
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    return errors;
+}
+
+
+
---
+++ test/index.js
@@ -0,0 +1,302 @@
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var expect = chai.expect;
+chai.use(sinonChai);
+
+var paqChangeOfBaseFR = require("../");
+
+describe("getConversion(randomStream, params, defaultValue)", function() {
+	var randomStreamMock, params;
+	var pickStub;
+	var defaultValueDouble;
+	beforeEach(function() {
+		randomStreamMock = { pick: function() {} };
+		defaultValueDouble = "defaultValue-double";
+		pickStub = sinon.stub(randomStreamMock, "pick");
+	});
+	afterEach(function() {
+		pickStub.restore();
+	});
+	describe("when no conversions are specified", function() {
+		beforeEach(function() {
+			params = { conversions: [] };
+		});
+		it("should call call pick with the defaultValue", function() {
+			paqChangeOfBaseFR.getConversion(randomStreamMock, params, defaultValueDouble);
+			expect(pickStub.calledOnce).to.be.true;
+			expect(pickStub.calledWith(defaultValueDouble)).to.be.true;
+		});
+	});
+	describe("when conversions are provided", function() {
+		var conversionsDouble;
+		beforeEach(function() {
+			conversionsDouble = [ "conversions", "double" ];
+			params = { conversions: conversionsDouble };
+		});
+		it("should call call pick with the provided conversions", function() {
+			paqChangeOfBaseFR.getConversion(randomStreamMock, params, defaultValueDouble);
+			expect(pickStub.calledOnce).to.be.true;
+			expect(pickStub.calledWith(conversionsDouble)).to.be.true;
+		});
+	});
+	describe("return value", function() {
+		it("should be the result of randomStream.pick", function() {
+			pickStub.returns("pick-return-double");
+			var returnValue = paqChangeOfBaseFR.getConversion(randomStreamMock, params, defaultValueDouble);
+			expect(returnValue).to.equal("pick-return-double");
+		});
+	});
+});
+
+describe("getSpaceBinary", function() {
+	describe("when spaceBinary is not defined in params", function() {
+		it("should return true as the default value", function() {
+			var res = paqChangeOfBaseFR.getSpaceBinary({});
+			expect(res).to.be.true;
+		});
+	});
+	describe("when params.spaceBinary is defined", function() {
+		it("should return that value", function() {
+			var res = paqChangeOfBaseFR.getSpaceBinary({ spaceBinary: "spaceBinary-double"});
+			expect(res).to.equal("spaceBinary-double");
+		});
+	});
+});
+
+// bad giant unit test...
+describe("generate(randomStream, params)", function() {
+	var res;
+	var numToConvertMock, randomStreamMock;
+	var randIntBetweenInclusiveStub, toStringStub, generateQuestionTextStub, getSpaceBinaryStub,
+		formatAnswerStub;
+	var fromRadDouble, toRadDouble, minDoube, maxDouble, paramsDouble, spaceBinaryDouble,
+		fromDouble, answerDouble, indexDouble, questionTextDouble, formatDouble;
+	var sandbox;
+	function setupSandbox() {
+		sandbox = sinon.sandbox.create();
+
+		numToConvertMock = { toString: function() { } };
+		randomStreamMock = { shuffle: function() { }, randIntBetweenInclusive: function() {} };
+		fromDouble = sandbox.spy();
+		answerDouble = sandbox.spy();
+		paramsDouble = sandbox.spy();
+		spaceBinaryDouble = sandbox.spy();
+		maxDouble = sandbox.spy();
+		minDouble = sandbox.spy();
+		formatDouble = sandbox.spy();
+		fromRadDouble = sandbox.spy();
+		toRadDouble = sandbox.spy();
+		questionTextDouble = sandbox.spy();
+		conversionDouble = {
+			radix: { from: fromRadDouble, to: toRadDouble },
+			range: { min: minDouble, max: maxDouble }
+		}
+
+		getConversionStub = sandbox.stub(paqChangeOfBaseFR, "getConversion").returns(conversionDouble);
+		randIntBetweenInclusiveStub = sandbox.stub(randomStreamMock, "randIntBetweenInclusive").returns(numToConvertMock);
+		toStringStub = sandbox.stub(numToConvertMock, "toString")
+			.withArgs(fromRadDouble).returns(fromDouble)
+			.withArgs(toRadDouble).returns(answerDouble);
+		getSpaceBinaryStub = sandbox.stub(paqChangeOfBaseFR, "getSpaceBinary").returns(spaceBinaryDouble);
+		generateQuestionTextStub = sandbox.stub(paqChangeOfBaseFR, "generateQuestionText").returns(questionTextDouble);
+		formatAnswerStub = sandbox.stub(paqChangeOfBaseFR, "formatAnswer").returns(formatDouble);
+	}
+	beforeEach(function() {
+		setupSandbox();
+		res = new paqChangeOfBaseFR.generate(randomStreamMock, paramsDouble);
+
+	});
+	afterEach(function() {
+		sandbox.restore();
+	});
+
+	describe("getConversion", function() {
+		it("should be called with appropriate parameters", function() {
+			expect(getConversionStub.calledOnce).to.be.true;
+			expect(getConversionStub.calledWith(
+				randomStreamMock, paramsDouble, paqChangeOfBaseFR.defaultConversions
+			)).to.be.true;
+		});
+	});
+	describe("getSpaceBinary", function() {
+		it("should be called and passed params", function() {
+			expect(getSpaceBinaryStub.calledOnce).to.be.true;
+			expect(getSpaceBinaryStub.calledWith(paramsDouble)).to.be.true;
+		});
+	});
+	describe("numToConvert", function() {
+		it("should be selected with randIntBetweenInclusive with min, max from conversion", function() {
+			expect(randIntBetweenInclusiveStub.calledOnce).to.be.true;
+			expect(randIntBetweenInclusiveStub.calledWith(minDouble, maxDouble)).to.be.true;
+		});
+	});
+	describe("number to convert string format", function() {
+		it("should be determined by toString(fromRad) on numToConvert", function() {
+			expect(toStringStub.withArgs(fromRadDouble).calledOnce).to.be.true;
+		});
+	});
+	describe("properties assigned to this", function() {
+		describe("answer", function() {
+			describe("when spaceBinary is false", function() {
+				it("should be determined by toString(toRad) on numToConvert", function() {
+					sandbox.restore();
+					setupSandbox();
+					getSpaceBinaryStub.returns(false);
+					res = new paqChangeOfBaseFR.generate(randomStreamMock, paramsDouble);
+					expect(toStringStub.withArgs(toRadDouble).calledOnce).to.be.true;
+					expect(res.answer).to.equal(answerDouble);
+				});
+			});
+			describe("when spaceBinary is true", function() {
+				it("should be the formattedResult", function() {
+					sandbox.restore();
+					setupSandbox();
+					getSpaceBinaryStub.returns(true);
+					res = new paqChangeOfBaseFR.generate(randomStreamMock, paramsDouble);
+					expect(toStringStub.withArgs(toRadDouble).calledOnce).to.be.true;
+					expect(formatAnswerStub.withArgs(answerDouble, fromRadDouble, toRadDouble).calledOnce).to.be.true;
+					expect(res.answer).to.equal(formatDouble);
+				});
+			});
+		});
+		describe("question", function() {
+			it("should be determined by generateQuestionText(randomStream, from, fromRad, toRad)", function() {
+				expect(generateQuestionTextStub.withArgs(
+					randomStreamMock, fromDouble, fromRadDouble, toRadDouble, spaceBinaryDouble
+				).calledOnce).to.be.true;
+			});
+			it("should be assigned to result of generateQuestionText", function() {
+				expect(res.question).to.equal(questionTextDouble);
+			});
+		});
+		describe("format", function() {
+			it("should be multiple-choice", function() {
+				expect(res.format).to.equal("free-response");
+			});
+		});
+	});
+});
+
+describe('radixDescription(radix, useBase)', function() {
+	describe('when useBase is true and radix is  8', function() {
+		it('should return "base 8"', function() {
+			expect(paqChangeOfBaseFR.radixDescription(8, true)).to.equal("base 8");
+		});
+	});
+	describe('when useBase is false', function() {
+		describe('when radix is 2', function() {
+			it('should return "binary"', function() {
+				expect(paqChangeOfBaseFR.radixDescription(2, false)).to.equal("binary");
+			});
+		});
+		describe('when radix is 8', function() {
+			it('should return "octal"', function() {
+				expect(paqChangeOfBaseFR.radixDescription(8, false)).to.equal("octal");
+			});
+		});
+		describe('when radix is 16', function() {
+			it('should return "hexadecimal"', function() {
+				expect(paqChangeOfBaseFR.radixDescription(16, false)).to.equal("hexadecimal");
+			});
+		});
+		describe('when radix is 10', function() {
+			it('should return "decimal"', function() {
+				expect(paqChangeOfBaseFR.radixDescription(10, false)).to.equal("decimal");
+			});
+		});
+		describe('when radix is 5', function() {
+			it('should return "base 5"', function() {
+				expect(paqChangeOfBaseFR.radixDescription(5, false)).to.equal("base 5");
+			});
+		});
+	});
+});
+
+describe("generateQuestionText(randomStream, from, fromRad, toRad, spaceBinary)", function() {
+	var res;
+	var randomStreamMock;
+	var nextIntRangeStub, radixDescriptionStub;
+	var fromDouble, fromRadDouble, toRadDouble;
+	beforeEach(function() {
+		randomStreamMock = {
+			nextIntRange: function() {}
+		}
+		fromDouble = "from-double";
+		fromRadDouble = "fromRad-double";
+		toRadDouble = "toRad-double";
+		nextIntRangeStub = sinon.stub(randomStreamMock, "nextIntRange");
+		nextIntRangeStub.withArgs(2)
+		.onFirstCall().returns("first-nextInt-double")
+		.onSecondCall().returns("second-nextInt-double");
+
+		radixDescriptionStub = sinon.stub(paqChangeOfBaseFR, "radixDescription");
+		radixDescriptionStub.withArgs(fromRadDouble, "first-nextInt-double").returns("from-description-double")
+		.withArgs(toRadDouble, "second-nextInt-double").returns("to-description-double");
+	});
+	afterEach(function() {
+		nextIntRangeStub.restore();
+		radixDescriptionStub.restore();
+	});
+
+	it('should call randomStream.nextIntRange with argument 2, twice', function() {
+		res = paqChangeOfBaseFR.generateQuestionText(randomStreamMock, fromDouble, fromRadDouble, toRadDouble);
+		expect(nextIntRangeStub.calledTwice).to.be.true;
+	});
+
+	it('should produce the random description for fromRad', function() {
+		res = paqChangeOfBaseFR.generateQuestionText(randomStreamMock, fromDouble, fromRadDouble, toRadDouble);
+		expect(radixDescriptionStub.withArgs(fromRadDouble, "first-nextInt-double").calledOnce).to.be.true;
+	});
+
+	it('should produce the random description for toRad', function() {
+		res = paqChangeOfBaseFR.generateQuestionText(randomStreamMock, fromDouble, fromRadDouble, toRadDouble);
+		expect(radixDescriptionStub.withArgs(toRadDouble, "second-nextInt-double").calledOnce).to.be.true;
+	});
+
+	it('should return the question text with appropriate radix formats', function() {
+		res = paqChangeOfBaseFR.generateQuestionText(randomStreamMock, fromDouble, fromRadDouble, toRadDouble);
+		expect(res).to.equal("Convert " + fromDouble + " from from-description-double to to-description-double.");
+	});
+
+	describe("when spaceBinary is true", function() {
+		var formatFromStub, formattedDouble;
+		beforeEach(function() {
+			formattedDouble = "formatted-double";
+			formatFromStub = sinon.stub(paqChangeOfBaseFR, "formatFrom").returns("formatted-double");
+		});
+		afterEach(function() {
+			formatFromStub.restore();
+		});
+		it("should format from", function() {
+			res = paqChangeOfBaseFR.generateQuestionText(randomStreamMock, fromDouble, fromRadDouble, toRadDouble, true);
+			expect(formatFromStub.calledOnce).to.be.true;
+			expect(formatFromStub.calledWith(fromDouble, fromRadDouble, toRadDouble)).to.be.true;
+			expect(res).to.equal("Convert " + formattedDouble + " from from-description-double to to-description-double.");
+		});
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
